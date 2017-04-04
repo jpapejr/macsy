d3617b6ce973269fd89ab7fd8f104a615f2401da
John Pape <jtpape@us.ibm.com>
2017-04-04 14:02:13 -0400

    Adding USER_HOME var and changing emacs install location

--- bootstrap.system.sh
+++ bootstrap.system.sh
@@ -1,6 +1,7 @@
 GIT_EMAIL=""
 GIT_NAME=""
 GIT_TOKEN=""
+USER_HOME=""
 
 # Install prereq Ubuntu packages
 sudo apt-get update && sudo apt-get install -y openjdk-8-jdk xvfb build-essential cmake python-dev python3-dev apt-transport-https ca-certificates curl software-properties-common bison mercurial make binutils gcc golang
@@ -28,12 +29,12 @@ git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
 
 #Set up the projects folder
-mkdir -p /home/jtpape/projects/node
-mkdir -p /home/jtpape/projects/java
-mkdir -p /home/jtpape/projects/go
-mkdir -p /home/jtpape/projects/ruby
-mkdir -p /home/jtpape/projects/python
-mkdir -p /home/jtpape/che
+mkdir -p /home/$USER_HOME/projects/node
+mkdir -p /home/$USER_HOME/projects/java
+mkdir -p /home/$USER_HOME/projects/go
+mkdir -p /home/$USER_HOME/projects/ruby
+mkdir -p /home/$USER_HOME/projects/python
+mkdir -p /home/$USER_HOME/che
 
 # Install grab Eclipse and Eclin and start it up
 cd ~
@@ -42,40 +43,40 @@ wget "https://github.com/ervandew/eclim/releases/download/2.6.0/eclim_2.6.0.jar"
 tar -zxf eclipse-jee-neon-2-linux-gtk-x86_64.tar.gz
 echo "osgi.instance.area.default=@user.home/projects/java" >> ~/.eclimrc
 Xvfb :1 -screen 0 1024x768x24 &
-java -Dvim.files=$HOME/.vim -Declipse.home=/home/jtpape/eclipse -jar eclim_2.6.0.jar install
+java -Dvim.files=$HOME/.vim -Declipse.home=/home/$USER_HOME/eclipse -jar eclim_2.6.0.jar install
 cd eclipse && DISPLAY=:1 ./eclipse/eclimd -b && cd -
 
 # Get Nave going for Node.js
 cd ~
 curl https://raw.githubusercontent.com/isaacs/nave/master/nave.sh > ~/nave.sh
-chmod +x ~/nave.sh && sudo ln -s /home/jtpape/nave.sh /usr/local/bin/nave
+chmod +x ~/nave.sh && sudo ln -s /home/$USER_HOME/nave.sh /usr/local/bin/nave
 sudo nave usemain 6.9.5
 
 # Get the tern.js project file in place
-cd /home/jtpape/macsy
-cp tern-project /home/jtpape/.tern-project
+cd /home/$USER_HOME/macsy
+cp tern-project /home/$USER_HOME/.tern-project
 
 # Set up emacs
-sudo add-apt-repository -y ppa:ubuntu-elisp
-sudo apt-get update
-sudo apt-get install emacs-snapshot
-cp emacs /home/jtpape/.emacs
+sudo apt-add-repository -y ppa:adrozdoff/emacs
+sudo apt update
+sudo apt install emacs25
+cp emacs /home/$USER_HOME/.emacs
 
 # ensure we have some eslint dependencies
 sudo npm install -g eslint
 sudo npm install -g babel-eslint
 
 # Shell fix-ups
-echo PATH="$HOME/bin:$HOME/.local/bin:/usr/local/go/bin:/home/jtpape/projects/go/bin:$PATH" >> /home/jtpape/.profile
-echo "export GHE_TOKEN=$GIT_TOKEN" >> /home/jtpape/.profile
-echo "export GOPATH=/home/jtpape/projects/go" >> /home/jtpape/.profile
+echo PATH="$HOME/bin:$HOME/.local/bin:/usr/local/go/bin:/home/$USER_HOME/projects/go/bin:$PATH" >> /home/$USER_HOME/.profile
+echo "export GHE_TOKEN=$GIT_TOKEN" >> /home/$USER_HOME/.profile
+echo "export GOPATH=/home/$USER_HOME/projects/go" >> /home/$USER_HOME/.profile
 mkdir ~/.bash
 cd ~/.bash
 git clone git://github.com/jimeh/git-aware-prompt.git
-echo "export GITAWAREPROMPT=~/.bash/git-aware-prompt" >> /home/jtpape/.bashrc
-echo "source \"${GITAWAREPROMPT}/main.sh\"" >> /home/jtpape/.bashrc
-echo 'export PS1="\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "' >> /home/jtpape/.bashrc
+echo "export GITAWAREPROMPT=~/.bash/git-aware-prompt" >> /home/$USER_HOME/.bashrc
+echo "source \"${GITAWAREPROMPT}/main.sh\"" >> /home/$USER_HOME/.bashrc
+echo 'export PS1="\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "' >> /home/$USER_HOME/.bashrc
 
 # TBD Eclipse Che
 sudo chmod ugo+rwx /var/run/docker.sock
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v /home/jtpape/che:/data eclipse/che start
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v /home/$USER_HOME/che:/data eclipse/che start
